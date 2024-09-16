1c546e220e672b8c35388619ef82ad92ab4457ff
vishal kumar <vishalkumar1301@gmail.com>
2024-09-16 16:58:57 -0400

    fixed build error

--- src/components/FetchTopHeadlines.tsx
+++ src/components/FetchTopHeadlines.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { MultiSelect } from "@/components/ui/multi-select"
 import { Article } from '@/lib/Article'
 
